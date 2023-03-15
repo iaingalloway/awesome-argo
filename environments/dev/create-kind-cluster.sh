d87f3f7d5f1e18c33fdc31a3300c6edba9bc6d43
Iain Galloway <iain.galloway@gmail.com>
2023-03-15 11:38:06 +0000

    create-kind-cluster.sh now waits on argo

--- environments/dev/create-kind-cluster.sh
+++ environments/dev/create-kind-cluster.sh
@@ -10,6 +10,11 @@ kind create cluster --config ./kind-config.yaml
 kubectl create namespace argocd
 kubectl apply --namespace argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
 
+# wait for argocd
+echo 'Waiting for argo...'
+kubectl wait --namespace argocd --for=condition=available deploy argocd-server --timeout=5m
+kubectl wait --namespace argocd --for=condition=ready pod -l app.kubernetes.io/name=argocd-server --timeout=5m
+
 # install awesome app of apps
 kubectl apply --namespace argocd -f ./awesome-applications.yaml
 
